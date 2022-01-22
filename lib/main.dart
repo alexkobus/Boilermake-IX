db2049c92e1578408d5f0cc058345668f657c93b
Grady Whelan <32005439+gradyw@users.noreply.github.com>
2022-01-21 23:39:20 -0500

    fix async bug

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:thoughtify/pages/import_pages.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
